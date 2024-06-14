44f9ea42965e9b6fbea899c70c5b83c74114b4b9
kwadjanib <115565495+kwadjanib@users.noreply.github.com>
2024-06-14 23:58:23 +0200

    Update Algo.java

--- src/main/java/iut/sae/algo/Algo.java
+++ src/main/java/iut/sae/algo/Algo.java
@@ -7,11 +7,6 @@ public class Algo{
         return "NotYetImplemented";
     }
 
-    public static String RLE(String in, int iteration) throws AlgoException{
-        // Provide your algo here
-        return "NotYetImplemented";
-    }
-
     public static String unRLE(String in) throws AlgoException{
         // Provide your algo here
         return "NotYetImplemented";
